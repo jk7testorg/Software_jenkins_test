24141c85088fe9a4bb246d1b0376d7278ac91ec1
Jaykumar Kesavan <jaykumar.k7@gmail.com>
2026-03-16 16:28:52 +0530

    Update server.js

--- template/server.js
+++ template/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.end('<body style="background:white;font-family:Arial;text-align:center;margin-top:40px;font-size:28px;">Welcome to Backstage SSP!</body>');
 });
 
