2f6b174fd3dca5294b6f86500d59f885ef347415
Jaykumar Kesavan <jaykumar.k7@gmail.com>
2026-03-16 10:53:39 +0530

    Update server.js

--- template/server.js
+++ template/server.js
@@ -1,22 +1,13 @@
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Welcome to ${{values.app_name}}\n");
+});
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, "index.html");
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Server error");
-      return;
-    }
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-});
-
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
 });
